8e02ea5938efafc7f06134757474f0830d62e5dd
Zarjij Hasan joy <rubayedhasan235@gmail.com>
2023-08-01 20:59:44 +0600

    function used

--- function.js
+++ function.js
@@ -1,5 +1,18 @@
+//  function
  function myBank (money){
     console.log(money,"tk");
  }
-
+// finction call
  myBank(500);
+ myBank(300);
+ myBank(100);
+
+
+ 
+// fintion withe pera meter
+ function add(x,y){
+    let ans = x + y;
+    console.log(ans,"tk");
+    return(ans);
+ }
+ add(500,500);
