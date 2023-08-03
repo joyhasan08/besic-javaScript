529cd2ecb0c6c24e7a93b2ff4cfd42e0e6fb84d5
Zarjij Hasan joy <rubayedhasan235@gmail.com>
2023-08-04 00:07:21 +0600

    odd and even number cheacker

---
+++ EvenOdd.js
@@ -0,0 +1,29 @@
+let number = 50;
+/*
+if(number % 2 == 0){
+    console.log('even');
+}
+else{
+    console.log('odd');
+}
+*/
+
+
+
+
+function evenOddCheacker (num1){
+    if(num1 % 2 == 0){
+        return true;
+    }
+    else{
+        return false;
+    }
+
+
+   
+}
+
+ let reasult = evenOddCheacker(number)
+ console.log(reasult);
+ reasult = evenOddCheacker(9)
+ console.log(reasult);
