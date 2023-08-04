7ddaf703b06be0b7ed6b7755516c2c207f21683d
Zarjij Hasan joy <rubayedhasan235@gmail.com>
2023-08-04 22:41:33 +0600

    sum of odd numbers

--- sumOfArry.js
+++ sumOfArry.js
@@ -1,6 +1,6 @@
-
-let givenArry = [10,23,44,54,78,12,];
-givenArry.push(100);
+// function of sum
+let givenArry = [10,23,44,54,78,12, 13 ,17,19];
+// givenArry.push(100);
 
 function getSumOfAnArry (arry){
     let sum = 0;
@@ -14,3 +14,24 @@ function getSumOfAnArry (arry){
 }
 const totalOfArry =  getSumOfAnArry(givenArry);
 console.log('sum of the fiven arry is', totalOfArry);
+
+//function of finding odd numbers
+ 
+function getOddNumbers (numbersOfArry) {
+    let foundOddNum =[];
+    for( let i= 0; i < numbersOfArry.length; i++){
+        let index = numbersOfArry[i];
+        //  console.log(index);
+         if( index % 2 == 1){
+            // console.log(index);
+            foundOddNum.push(index);
+         }
+    }
+    // console.log(foundOddNum);
+    return foundOddNum;
+}
+
+let oddNum = getOddNumbers(givenArry);
+console.log(oddNum);
+let sumOfOddNum = getSumOfAnArry(oddNum);
+console.log('sum off odd number from the given arry', sumOfOddNum);
