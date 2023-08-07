c682e48a3becca07e3826864b2d61ddb854969f8
Zarjij Hasan joy <rubayedhasan235@gmail.com>
2023-08-08 02:55:36 +0600

    min max

---
+++ maxMin.js
@@ -0,0 +1,19 @@
+let a = 10;
+let b = 200;
+let c = 40;
+
+function  maxNumber (i,j,k) {
+    
+    const result = Math.max(a,b,c);
+    return result;
+
+}
+
+console.log(maxNumber(a,b,c));
+
+function minNumber(x,y,z) {
+    const isMinNumber = Math.min(x,y,z);
+    return isMinNumber;
+}
+console.log(minNumber(a,b,c));
+ 
