7c4979cf69fa6683ae93fcb2943f65f8dcba7409
Zarjij Hasan joy <rubayedhasan235@gmail.com>
2023-08-07 15:34:49 +0600

    string slice

---
+++ strng-slice.js
@@ -0,0 +1,13 @@
+
+let lyrics =  ' Amay deko na, ferano jabe nah , ferari paki ra ';
+
+let lattersOfLyrics = lyrics.split('');
+console.log(lattersOfLyrics);
+let wordsOfLyrics = lyrics.split(' ');
+console.log(wordsOfLyrics);
+let sentenceOfLyrics = lyrics.split(',');
+console.log(sentenceOfLyrics);
+
+let sliceOfLyrics = lyrics.slice(0,20); // start from 0 index end at 19 1index
+console.log(sliceOfLyrics);
+
