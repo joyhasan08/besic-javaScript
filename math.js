ebf57fc2ec028f05d7ba58c385d0f871931e5fdc
Zarjij Hasan joy <rubayedhasan235@gmail.com>
2023-08-07 15:41:43 +0600

    matth of js

--- math.js
+++ math.js
@@ -1,2 +1,10 @@
+//power method
 const result = Math.pow(2,4);
 console.log(result);
+
+let age1 = 18;
+let age2 = 25;
+
+let ageGap = age1 -age2;
+let absoluteAge = Math.abs(ageGap);
+console.log(absoluteAge);
