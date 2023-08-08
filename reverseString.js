064288b91ddca09dbb2a780687af8b42e941200c
Zarjij Hasan joy <rubayedhasan235@gmail.com>
2023-08-09 00:18:13 +0600

    problem 2 solved

---
+++ reverseString.js
@@ -0,0 +1,19 @@
+
+// Problem - 2 
+
+let  strg1 = 'John Doe'
+let  strg2 = 'ohn'
+
+function matchFinder(string1, string2) {
+
+    
+    let result = string1.includes(string2);
+    return result;
+ }
+
+
+console.log(matchFinder(strg1,strg2));
+
+//////////////////////////////////////////////
+
+
