e8f996d96efddc6b53af6a53c3d9f29bc2e11171
Zarjij Hasan joy <rubayedhasan235@gmail.com>
2023-08-09 15:38:55 +0600

    problem 3

---
+++ Problem3-As4.js
@@ -0,0 +1,25 @@
+
+// done
+
+let inputArry =[12,12];
+
+function sortMaker(arr) {
+    if(arr[0] < 0 || arr[1] < 0){
+        return'Invalid number'
+    }
+    else if(arr[0] >= 0 && arr[1] >= 0 && arr[1] > arr[0]){
+        let maxNumber = arr[1];
+        let minNumber = arr[0];
+        let newArry = [ maxNumber, minNumber];
+        return newArry;
+    }
+    else if ( arr[0] > arr[1]){
+        return arr;
+    }
+    else{
+        return 'equal'
+    }
+       
+ }
+ console.log(sortMaker(inputArry));
+
--- reverseString.js
+++ reverseString.js
@@ -1,14 +1,21 @@
 
 // Problem - 2 
 
-let  strg1 = 'John Doe'
-let  strg2 = 'ohn'
+let  strg1 = 'joy hasan ';
+let  strg2 = 'oyasdas'
 
 function matchFinder(string1, string2) {
 
+    if ( typeof string1 !== "string" || typeof string2 !== "string" ){
+        console.log("Erro. Please Enter a String");
+    }
+    else{
+        let result = string1.includes(string2);
+        return result;
+    }
     
-    let result = string1.includes(string2);
-    return result;
+    
+    
  }
 
 
